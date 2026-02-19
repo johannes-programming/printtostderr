8a61795b395093c32eb1b8e90a49a1815f321f71
Johannes <johannes-programming@mailfence.com>
2026-02-19 23:39:09 +0100

    Version 1.0.2

--- src/printtostderr/core/__init__.py
+++ src/printtostderr/core/__init__.py
@@ -6,7 +6,7 @@ __all__ = ["main", "printtostderr"]
 
 
 def main(args: Optional[Iterable] = None) -> None:
-    """This function parses the args and hands them to the printtostderr function."""
+    "This function parses the args and hands them to the printtostderr function."
     if args is not None:
         args = [str(x) for x in args]
     parser = argparse.ArgumentParser()
