5f43efd15b5638ba833d774b5b81df97459d3356
Johannes <johannes.programming@gmail.com>
2026-05-14 10:18:26 +0200

    Version 1.1.1

--- src/printtostderr/core/__init__.py
+++ src/printtostderr/core/__init__.py
@@ -7,18 +7,13 @@ __all__ = ["main", "printtostderr"]
 
 def main(args: Optional[Iterable] = None) -> None:
     "This function parses the args and hands them to the printtostderr function."
-    args_: Iterable
     parser: argparse.ArgumentParser
     space: argparse.Namespace
-    if args is None:
-        args_ = args
-    else:
-        args_ = list(map(str, args))
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "args", nargs="*", help="These arguments will be printed to sys.stderr."
     )
-    space = parser.parse_args(args_)
+    space = parser.parse_args(args)
     printtostderr(*space.args)
 
 
